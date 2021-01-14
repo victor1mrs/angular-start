95324699c3669dee524afe033ed5f9411b752617
victor1mrs <victor2mrs@hotmail.com>
2021-01-14 00:04:19 -0300

    Usando formulario para inputs de usuario

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 import { CartService } from "../cart.service";
 
 @Component({
@@ -7,8 +8,22 @@ import { CartService } from "../cart.service";
   styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
-  items;
-  constructor(private cartService: CartService) {}
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: "",
+    address: ""
+  });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn("Your order has been submitted", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
